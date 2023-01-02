647d7c35bf99ac5d25480ef4d4273b4529dcb22f
Stefano Ubbiali <subbiali@phys.ethz.ch>
2023-01-02 14:20:18 +0100

    MLux: add script to launch FVM on CPUs.

---
+++ meluxina/run_fvm_cpu.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -l
+
+BRANCH=${BRANCH:-distributed}
+GT_BACKEND=${GT_BACKEND:-dace:gpu}
+NUM_NODES=${NUM_NODES:-1}
+NUM_RUNS=${NUM_RUNS:-5}
+NUM_TASKS=${NUM_TASKS:-1}
+NUM_THREADS=${NUM_THREADS:-16}
+OVERCOMPUTING=${OVERCOMPUTING:-0}
+USE_CASE=${USE_CASE:-thermal}
+
+#if [ "$NUM_TASKS" -gt "$NUM_NODES" ]; then
+#  NUM_TASKS_PER_NODE=2
+#else
+#  NUM_TASKS_PER_NODE=1
+#fi
+
+. prepare_fvm.sh
+pushd "$FVM" || return
+. venv/bin/activate
+pushd drivers || return
+
+export UCX_MAX_RNDV_RAILS=1
+
+for i in $(eval echo "{1..$NUM_RUNS}"); do
+  echo "NUM_NODES=$NUM_NODES: start"
+  GT_BACKEND="$GT_BACKEND" \
+    FVM_ENABLE_BENCHMARKING=1 \
+    FVM_ENABLE_OVERCOMPUTING="$OVERCOMPUTING" \
+    OMP_NUM_THREADS="$NUM_THREADS" \
+    OMP_PLACES=cores \
+    OMP_PROC_BIND=close \
+    srun \
+      --nodes="$NUM_NODES" \
+      --ntasks="$NUM_TASKS" \
+      --ntasks-per-node="$NUM_TASKS_PER_NODE" \
+      --cpus-per-task=32 \
+      python run_model.py \
+        ../config/weak-scaling/"$USE_CASE"/"$NUM_TASKS".yml \
+        --csv-file=../data/meluxina/weak-scaling/"$USE_CASE"/"$NUM_TASKS".csv
+  echo "NUM_NODES=$NUM_NODES, i=$i.1: end"
+  echo ""
+done
+
+popd || return
+popd || return
--- meluxina/submit_fvm.sh
+++ meluxina/submit_fvm.sh
@@ -3,13 +3,14 @@
 set -a
 
 BRANCH=${BRANCH:-distributed}
-GT_BACKEND_L=( dace:gpu )
+GT_BACKEND_L=( gt:cpu_kfirst )
+JOB_SCRIPT=run_fvm_cpu.sh
 NUM_NODES_L=( 1 2 )
-NUM_RUNS=${NUM_RUNS:-8}
-NUM_TASKS_PER_NODE=${NUM_TASKS_PER_NODE:-4}
+NUM_RUNS=4
+NUM_TASKS_PER_NODE=2
 OVERCOMPUTING_L=( 0 )
 TIME=${TIME:-01:00:00}
-USE_CASE_L=( thermal-large-periodic baroclinic-wave-sphere baroclinic-wave-sphere-unsplit )
+USE_CASE_L=( thermal-large )
 
 for USE_CASE in "${USE_CASE_L[@]}"; do
   for GT_BACKEND in "${GT_BACKEND_L[@]}"; do
@@ -17,7 +18,6 @@ for USE_CASE in "${USE_CASE_L[@]}"; do
       for NUM_NODES in "${NUM_NODES_L[@]}"; do
         NUM_TASKS=$(( NUM_NODES * NUM_TASKS_PER_NODE ))
         JOB_NAME="$USE_CASE-$NUM_NODES-$NUM_TASKS" \
-          JOB_SCRIPT=run_fvm_gpu.sh \
           . submit.sh
       done
     done
