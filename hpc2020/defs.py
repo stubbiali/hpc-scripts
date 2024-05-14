3e5e9cb30d8e86e4a572ee0ac5394f7ece3bb90f
stubbiali <subbiali@phys.ethz.ch>
2024-05-14 14:12:55 +0200

    Review defs.

--- hpc2020/defs.py
+++ hpc2020/defs.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+import os
 import typing
 
 FloatingPointPrecision = typing.Literal["double", "single"]
-Partition = typing.Literal["gpu"]
-ProgrammingEnvironment = typing.Literal["gnu"]
+MPI = typing.Literal["hpcx", "intelmpi", "openmpi"]
+Partition = typing.Literal["gpu", "par"]
+ProgrammingEnvironment = typing.Literal["gnu", "intel"]
 
+valid_mpi_libraries = typing.get_args(MPI)
 valid_programming_environments = typing.get_args(ProgrammingEnvironment)
+
+root_dir = os.environ.get("HPCPERM", f"/home/{os.getlogin()}")
