21e09ce23a30fd18ae2234d56a6da5fbfe37c43f
stubbiali <subbiali@phys.ethz.ch>
2024-08-08 16:11:57 +0200

    daint: remove leftover.

--- daint/sbatch_pmapl.py
+++ daint/sbatch_pmapl.py
@@ -53,46 +53,45 @@ def core():
         use_case_l.keys(),
     ):
         for threads_layout in use_case_l[use_case]:
-            for seed in range(6, 13):
-                with utils.batch_directory():
-                    # job_name = (
-                    #     f"{use_case.replace('/', '_')}-{env}-{gt_backend}-{pmap_precision}-"
-                    #     f"{pmap_enable_overcomputing}-{threads_layout.num_nodes}-"
-                    #     f"{threads_layout.num_tasks_per_node}"
-                    # )
-                    # job_name = (
-                    #     f"{gt_backend}-{pmap_precision}-"
-                    #     f"{pmap_enable_overcomputing}-{threads_layout.num_nodes}-"
-                    #     f"{threads_layout.num_tasks_per_node}"
-                    # )
-                    # job_name = f"{use_case[45:52]}-{gt_backend}-{pmap_precision}-{threads_layout.num_nodes}"
-                    job_name = f"{use_case.replace('/', '-')}-{pmap_precision[0]}"
-                    job_script = generate_run_pmapl.core(
-                        branch=branch,
-                        env=env,
-                        ghex_aggregate_fields=ghex_aggregate_fields,
-                        ghex_collect_statistics=ghex_collect_statistics,
-                        gt_backend=gt_backend,
-                        num_nodes=threads_layout.num_nodes,
-                        num_runs=num_runs,
-                        num_tasks_per_node=threads_layout.num_tasks_per_node,
-                        num_threads_per_task=threads_layout.num_threads_per_task,
-                        partition=partition,
-                        pmap_disable_log=pmap_disable_log,
-                        pmap_enable_benchmarking=pmap_enable_benchmarking,
-                        pmap_enable_overcomputing=pmap_enable_overcomputing,
-                        pmap_precision=pmap_precision,
-                        use_case=use_case,
-                    )
-                sbatch.core(
-                    account=account,
-                    job_name=job_name,
-                    job_script=job_script,
+            with utils.batch_directory():
+                # job_name = (
+                #     f"{use_case.replace('/', '_')}-{env}-{gt_backend}-{pmap_precision}-"
+                #     f"{pmap_enable_overcomputing}-{threads_layout.num_nodes}-"
+                #     f"{threads_layout.num_tasks_per_node}"
+                # )
+                # job_name = (
+                #     f"{gt_backend}-{pmap_precision}-"
+                #     f"{pmap_enable_overcomputing}-{threads_layout.num_nodes}-"
+                #     f"{threads_layout.num_tasks_per_node}"
+                # )
+                # job_name = f"{use_case[45:52]}-{gt_backend}-{pmap_precision}-{threads_layout.num_nodes}"
+                job_name = f"{use_case.replace('/', '-')}-{pmap_precision[0]}"
+                job_script = generate_run_pmapl.core(
+                    branch=branch,
+                    env=env,
+                    ghex_aggregate_fields=ghex_aggregate_fields,
+                    ghex_collect_statistics=ghex_collect_statistics,
+                    gt_backend=gt_backend,
                     num_nodes=threads_layout.num_nodes,
+                    num_runs=num_runs,
                     num_tasks_per_node=threads_layout.num_tasks_per_node,
+                    num_threads_per_task=threads_layout.num_threads_per_task,
                     partition=partition,
-                    time=time,
+                    pmap_disable_log=pmap_disable_log,
+                    pmap_enable_benchmarking=pmap_enable_benchmarking,
+                    pmap_enable_overcomputing=pmap_enable_overcomputing,
+                    pmap_precision=pmap_precision,
+                    use_case=use_case,
                 )
+            sbatch.core(
+                account=account,
+                job_name=job_name,
+                job_script=job_script,
+                num_nodes=threads_layout.num_nodes,
+                num_tasks_per_node=threads_layout.num_tasks_per_node,
+                partition=partition,
+                time=time,
+            )
 
 
 if __name__ == "__main__":
