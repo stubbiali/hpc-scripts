05f517b065cd08bedd061fd59f5518e1340cf697
stubbiali <subbiali@phys.ethz.ch>
2024-09-26 10:12:21 +0200

    lumi: add defaults.

---
+++ lumi/defaults.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from typing import Optional
+
+import defs
+
+
+ACCOUNT: int = 465000527
+ENV: defs.ProgrammingEnvironment = "cray"
+GHEX_TRANSPORT_BACKEND: defs.GHEXTransportBackend = "libfabric"
+HDF5_VERSION: str = "1.14.4.3"
+NETCDF_VERSION: str = "4.9.2"
+PARTITION: defs.Partition = "standard-g"
+ROCM_VERSION: str = "6.0.3"
+STACK: defs.SoftwareStack = "lumi"
+STACK_VERSION: Optional[str] = "24.03"
