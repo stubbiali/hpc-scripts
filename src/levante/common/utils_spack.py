47800cdb8c98dfd2e9a623132710a3d8617866a0
stubbiali <subbiali@phys.ethz.ch>
2025-11-11 21:53:28 +0100

    Replace soft links with copies

--- src/levante/common/utils_spack.py
+++ src/levante/common/utils_spack.py
@@ -1,1 +1,30 @@
-/Users/subbiali/Desktop/kilos/hpc-scripts/main/src/common/utils_spack.py
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from . import utils
+
+if TYPE_CHECKING:
+    from typing import Optional
+
+
+def spack_install(spec: str, compiler: str, dependencies: Optional[list[str]] = None) -> None:
+    command = f"spack install {spec} %{compiler}"
+    dependencies = dependencies or []
+    for dependency in dependencies:
+        command += f" ^{dependency}"  # %{compiler}"
+    utils.run(command)
+
+
+def spack_load(
+    spec: str, compiler: Optional[str] = None, dependencies: Optional[list[str]] = None
+) -> None:
+    command = f"spack load {spec}{f' %{compiler}' if compiler else ''}"
+    dependencies = dependencies or []
+    for dependency in dependencies:
+        command += f" ^{dependency}{f' %{compiler}' if compiler else ''}"
+    utils.run(command)
+
+
+def spack_unload() -> None:
+    utils.run(f"spack unload --all")
--- src/lumi/common/utils.py
+++ src/lumi/common/utils.py
@@ -1,1 +1,130 @@
-/Users/subbiali/Desktop/kilos/hpc-scripts/main/src/common/utils.py
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+import contextlib
+import dataclasses
+import os
+import shutil
+import subprocess
+import tempfile
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Any, Optional
+
+
+BATCH_DIRECTORY_REGISTRY = []
+BATCH_FILE_REGISTRY = []
+
+
+@contextlib.contextmanager
+def batch_directory(path: Optional[str] = None):
+    assert len(BATCH_DIRECTORY_REGISTRY) <= 1
+    try:
+        if len(BATCH_DIRECTORY_REGISTRY) > 0:
+            final_cleanup = False
+            yield BATCH_DIRECTORY_REGISTRY[-1]
+        else:
+            final_cleanup = True
+            if path is not None:
+                path = os.path.abspath(path)
+                if os.path.exists(path):
+                    shutil.rmtree(path)
+                    print(f"py-hpc-scripts: overwrite {path}")
+                else:
+                    print(f"py-hpc-scripts: create {path}")
+                os.makedirs(path)
+            else:
+                os.makedirs("_tmp", exist_ok=True)
+                path = os.path.abspath(tempfile.mkdtemp(dir="_tmp"))
+                os.makedirs(path, exist_ok=True)
+                print(f"py-hpc-scripts: create {path}")
+            BATCH_DIRECTORY_REGISTRY.append(path)
+            yield path
+    finally:
+        if final_cleanup:
+            BATCH_DIRECTORY_REGISTRY.pop()
+
+
+@contextlib.contextmanager
+def batch_file(filename: Optional[str] = None):
+    if filename is not None:
+        if len(BATCH_DIRECTORY_REGISTRY) > 0:
+            fname = os.path.abspath(os.path.join(BATCH_DIRECTORY_REGISTRY[-1], filename + ".sh"))
+        else:
+            # os.makedirs("_tmp", exist_ok=True)
+            # fname = os.path.abspath(tempfile.mktemp(prefix=prefix + "_", suffix=".sh", dir="_tmp"))
+            fname = os.path.abspath(filename + ".sh")
+
+        try:
+            with open(fname, "w") as f:
+                BATCH_FILE_REGISTRY.append(f)
+                f.write("#!/bin/bash -l\n\n")
+                yield f, fname
+        finally:
+            print(f"py-hpc-scripts: write {fname}")
+            BATCH_FILE_REGISTRY.pop()
+
+
+def run(*args: str, split: bool = False, verbose: bool = False) -> None:
+    split_args = [item for arg in args for item in arg.split(" ")]
+    if split:
+        command = split_args[0]
+        for arg in split_args[1:]:
+            command += " \\\n    " + arg
+    else:
+        command = " ".join(split_args)
+    if verbose:
+        print(command)
+    if len(BATCH_FILE_REGISTRY) > 0:
+        BATCH_FILE_REGISTRY[-1].write(command + "\n")
+    else:
+        subprocess.run(command, capture_output=False, shell=True)
+
+
+class InvalidArgumentError(Exception):
+    def __init__(self, name: str, token: str, options: list[str]):
+        options = [f"`{opt}`" for opt in options]
+        msg = (
+            f"Invalid value `{token}` for parameter `{name}`. "
+            f"Available options: {', '.join(options)}."
+        )
+        super().__init__(msg)
+
+
+@contextlib.contextmanager
+def check_argument(name, token, options):
+    if token not in options:
+        raise InvalidArgumentError(name, token, options)
+    try:
+        yield token
+    finally:
+        pass
+
+
+def export_variable(name: str, value: Any) -> None:
+    run(f"export {name}={str(value)}")
+
+
+def append_to_path(path: str, value: Any) -> None:
+    run(f"{path}={str(value)}:${path}")
+
+
+@contextlib.contextmanager
+def chdir(dirname: str, restore: bool = True) -> None:
+    try:
+        run(f"pushd {dirname}")
+        yield None
+    finally:
+        if restore:
+            run("popd")
+
+
+@dataclasses.dataclass
+class ThreadsLayout:
+    num_nodes: int
+    num_tasks_per_node: int
+    num_threads_per_task: int
+
+    @property
+    def num_tasks(self) -> int:
+        return self.num_nodes * self.num_tasks_per_node
