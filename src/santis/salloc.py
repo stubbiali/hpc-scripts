5b3c99d338a02f81a37d7cf7f6be00d2e9318090
stubbiali <subbiali@phys.ethz.ch>
2026-02-19 18:14:02 +0100

    santis: clean-up salloc

--- src/santis/salloc.py
+++ src/santis/salloc.py
@@ -6,17 +6,14 @@ import argparse
 from typing import TYPE_CHECKING
 
 import common
-import defaults
 
 if TYPE_CHECKING:
     import defs
 
 
 # >>> config: start
-ACCOUNT: defs.Account = "c46"
 NUM_NODES: int = 1
 NUM_TASKS_PER_NODE: int = 4
-PARTITION: defs.Partition = "normal"
 TIME: str = "02:00:00"
 # >>> config: end
 
@@ -37,10 +34,10 @@ def core(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Get an allocation on the compute nodes.")
-    parser.add_argument("--account", type=str, default=ACCOUNT)
+    parser.add_argument("--account", type=str, default=defaults.ACCOUNT)
     parser.add_argument("--num-nodes", type=int, default=NUM_NODES)
     parser.add_argument("--num-tasks-per-node", type=int, default=NUM_TASKS_PER_NODE)
-    parser.add_argument("--partition", type=str, default=PARTITION)
+    parser.add_argument("--partition", type=str, default=defaults.PARTITION)
     parser.add_argument("--time", type=str, default=TIME)
     args = parser.parse_args()
     core(**args.__dict__)
