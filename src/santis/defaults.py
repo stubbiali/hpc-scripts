fe5f34f424a6216f34c68715cc7ce0ce2cd3e5d1
stubbiali <subbiali@phys.ethz.ch>
2026-02-19 18:04:34 +0100

    santis: add defaults

---
+++ src/santis/defaults.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import defs
+
+ACCOUNT: defs.Account = "c46"
+GHEX_TRANSPORT_BACKEND: defs.GHEXTransportBackend = "mpi"
+PARTITION: defs.Partition = "normal"
+PYTHON_VERSION: defs.PythonVersion = "3.14"
+UENV: defs.UEnv = "prgenv-gnu/24.11:v2"
