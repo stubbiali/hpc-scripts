ea86ccf3c01ace117adde28d233a3172af4c8678
stubbiali <subbiali@phys.ethz.ch>
2026-05-11 16:59:45 +0200

    lumi: update defaults

--- src/lumi/defaults.py
+++ src/lumi/defaults.py
@@ -3,14 +3,14 @@ from typing import Optional
 
 import defs
 
-
 ACCOUNT: int = 465000527
 ENV: defs.ProgrammingEnvironment = "cray"
 GHEX_TRANSPORT_BACKEND: defs.GHEXTransportBackend = "mpi"
-HDF5_VERSION: str = "1.14.4.3"
-NCO_VERSION: str = "5.3.4"
-NETCDF_VERSION: str = "4.9.2"
+HDF5_VERSION: str = "2.1.1"
+NCO_VERSION: str = "5.3.9"
+NETCDF_VERSION: str = "4.10.0"
 PARTITION: defs.Partition = "standard-g"
-ROCM_VERSION: str = "6.0.3"
+PYTHON_VERSION: defs.PythonVersion = "cray-python"
+ROCM_VERSION: str = "6.3.4"
 STACK: defs.SoftwareStack = "lumi"
-STACK_VERSION: Optional[str] = "24.03"
+STACK_VERSION: Optional[str] = "25.03"
