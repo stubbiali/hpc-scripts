7a9fb8d22ef052d3dc0ff9fa574b657349721b1c
stubbiali <subbiali@phys.ethz.ch>
2026-05-20 09:12:12 +0200

    Refactor hpc_scripts_common namespace package

--- hpc-scripts-common/src/hpc_scripts/common/utils_module.py
+++ hpc-scripts-common/src/hpc_scripts/common/utils_module.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import annotations
 
-from . import utils
+from hpc_scripts.common import utils
 
 
 def module_purge(force: bool = False) -> None:
--- hpc-scripts-common/src/hpc_scripts/common/utils_spack.py
+++ hpc-scripts-common/src/hpc_scripts/common/utils_spack.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from . import utils
+from hpc_scripts.common import utils
 
 if TYPE_CHECKING:
     from typing import Optional
