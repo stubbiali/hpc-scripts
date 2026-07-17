8e3df75dcb0f26fc05c922a646b29792b27d859f
stubbiali <subbiali@phys.ethz.ch>
2026-07-17 06:56:33 +0200

    leonardo/make_select_gpu: pin HCAs

--- hpc-scripts-leonardo/src/hpc_scripts/leonardo/make_select_gpu.py
+++ hpc-scripts-leonardo/src/hpc_scripts/leonardo/make_select_gpu.py
@@ -11,6 +11,7 @@ from hpc_scripts import common
 def core() -> str:
     with common.utils.output_file(filename="select_gpu") as (_, fname):
         common.utils.run("export CUDA_VISIBLE_DEVICES=$SLURM_LOCALID")
+        common.utils.run("export UCX_NET_DEVICES=mlx5_$SLURM_LOCALID:1")
         common.utils.run("exec $*")
     os.chmod(fname, stat.S_IRWXU)
     return fname
